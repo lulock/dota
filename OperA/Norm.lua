1e5bfc74f77945c849ffc63ed88c4c008e1a5462
Leila Methnani <leilamethnani@gmail.com>
2021-12-16 17:18:14 +0400

    updated scene and norm specifications

--- OperA/Norm.lua
+++ OperA/Norm.lua
@@ -19,27 +19,27 @@ Norm =  Class{ }
 
 -- what does opera say about doing a job WELL? so, maybe the agent will execute an action, but fail. is there a sanction? should be.
 
-function Norm:init(name, planner, behaviour, operator)
+function Norm:init(name, plan, behaviour, operator)
     self.name = name -- unique name id
-    self.planner = planner -- pointer to bot (or rather bot's plan??) 
+    self.plan = plan -- pointer to bot (or rather bot's plan??) 
     self.behaviour = behaviour -- maybe this should be drive ID?
     self.operator = operator -- OBLIGED / PERMITTED / TODO: NOT PERMITTED
 end
 
 function Norm:validate()
-    --print(self.planner, 'is', self.operator, 'to', self.behaviour)
+    --print(self.plan, 'is', self.operator, 'to', self.behaviour)
 
-    if self.planner.root.currentDriveName ~= self.behaviour then
+    if self.plan.root.currentDriveName ~= self.behaviour then
         --print ('NORM VIOLATION')
 
         -- ping at bot location where norm has been violated!! 
         local loc = GetBot():GetLocation()
         -- GetBot():ActionImmediate_Ping(loc.x, loc.y, true)
-        print ('Current behaviour set to:', self.planner.root.currentDriveName,'but it should be ', self.behaviour)
+        print ('Current behaviour set to:', self.plan.root.currentDriveName,'but it should be ', self.behaviour)
         return false
     else
         --print ('behaviour approved')
-        print ('Current behaviour set to:', self.planner.root.currentDriveName,'which aligns with ', self.behaviour)
+        print ('Current behaviour set to:', self.plan.root.currentDriveName,'which aligns with ', self.behaviour)
         return true
     end
     -- maybe this should check obligation / permission against agent's active drive?
@@ -47,15 +47,16 @@ end
 
 -- constrain to expected behaviour
 function Norm:sanction()
-    
-    for i,drive in pairs(self.planner.root.drives) do
+
+    for i,drive in pairs(self.plan.root.drives) do
         if drive.name == self.behaviour then
             --print('drive is', d.name)
-            norm.planner.root:removeDrive(i) -- remove the drive
-            norm.planner.root:insertDrive(drive, 1) -- re-insert drive as priority # 1
+            self.plan.root:removeDrive(i) -- remove the drive
+            self.plan.root:insertDrive(drive, 1) -- re-insert drive as priority # 1
             -- log role, time of change, and name of new priority drive to console
             print(POSITIONS[GetBot():GetUnitName()], ', ', DotaTime(),', ', drive.name) 
             -- these console logs are dumped into a text file by steam. Postprocess file by tokenising on [VScript] and then the rest should be CSV format.
         end -- TODO: handle if not found
     end
+    return
 end
--- OperA/Scene.lua
+++ OperA/Scene.lua
@@ -1,18 +1,61 @@
 Scene = Class{ }
 
-function Scene:init(name, roles, landmarks, results, rules)
+function Scene:init(name, roles, landmarks, results, rules, plan)
     self.name = name
     self.roles = roles -- multiple roles involved? or only current agent this scene is running on ...
     self.landmarks = landmarks -- sense
     self.results = results -- sense
     self.rules = rules -- Rule object, condition with norm behaviour expectation
-    -- self.status = false
-    self.prevPlanner = nil
+    self.status = IDLE
+    self.prevPlan = nil
+    self.plan = plan
 end
 
 function Scene:update() -- should a scene exit be determined by landmark?? 
     
-    -- if status is IDLE, then check landmarks
+    if self.status == IDLE then
+        for _, landmark in pairs(self.landmarks) do
+            -- has the scene begun? This is a world sense
+            if not landmark:tick() then
+                --print('landmark not sensed. return false')
+                return false 
+            end
+        end
+        self.status = RUNNING
+        print('scene running, check rules')
+
+        for _, rule in pairs(self.rules) do -- check rules
+            local norm = rule:tick() -- get norm / expected behaviour
+            local legal = norm:validate() -- check if in compliance with norm
+
+            if not legal then -- if agent in violation
+                print('norm', norm.name, 'will impose SANCTIONS!')
+
+                self.prevPlan = copy1(self.plan)
+                norm:sanction()
+
+                printTable(self.plan.root.drives)
+
+            end
+        end
+
+        return true
+    elseif self.status == RUNNING then
+        for _, result in pairs(self.results) do
+            if not result:tick() then -- if result still not met
+                return false
+            end
+        end
+        
+        self.plan = copy1(self.prevPlan) -- reset
+        self.prevPlan = nil -- reset 
+        self.status = IDLE -- reset
+        
+        print('scene complete, reset to idle')
+        printTable(self.plan.root.drives)
+
+        return true
+    end
 
         -- if all landmarks sensed, set to RUNNING and check norms
             -- if norms violated impose sanctions
@@ -21,7 +64,7 @@ function Scene:update() -- should a scene exit be determined by landmark??
 
     -- elseif status is RUNNING, then check all results
         
-        -- if all results sensed then reset planner and set status to IDLE
+        -- if all results sensed then revert to prev plan and set status to IDLE
     return false
     
 end
@@ -48,11 +91,11 @@ function Scene:OldUpdate()
                     -- impose sanction (e.g. alter plan)
                     print('norm', norm.name, 'will impose SANCTIONS!')
                     -- for now sanction is to switch expected drive and current drive
-                    self.prevPlanner = deepcopy(norm.planner)
+                    self.prevPlan = deepcopy(norm.plan)
                     norm:sanction()
 
-                    print('planner now looks like')
-                    printTable(norm.planner.root.drives)
+                    print('plan now looks like')
+                    printTable(norm.plan.root.drives)
                     return false -- not sure if false should be returned here
                 end
             end
@@ -81,11 +124,11 @@ end
 -- end
 
 function Scene:sanction()
-    for i,d in pairs(norm.planner.root.drives) do
+    for i,d in pairs(norm.plan.root.drives) do
         if d.name == norm.behaviour then
             --print('drive is', d.name)
-            norm.planner.root:removeDrive(i) -- remove the drive
-            norm.planner.root:insertDrive(d, 1) -- re-insert drive as priority # 1
+            norm.plan.root:removeDrive(i) -- remove the drive
+            norm.plan.root:insertDrive(d, 1) -- re-insert drive as priority # 1
             -- log role, time of change, and name of new priority drive to console
             print(POSITIONS[GetBot():GetUnitName()], ', ', DotaTime(),', ', d.name) 
             -- these console logs are dumped into a text file by steam. Postprocess file by tokenising on [VScript] and then the rest should be CSV format.
@@ -107,4 +150,11 @@ function deepcopy(orig)
         copy = orig
     end
     return copy
+end
+
+function copy1(obj)
+    if type(obj) ~= 'table' then return obj end
+    local res = {}
+    for k, v in pairs(obj) do res[copy1(k)] = copy1(v) end
+    return res
 end
